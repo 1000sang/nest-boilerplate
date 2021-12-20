c0fe10409356db1c80a4020a606e661c8626ce85
KmovSangHee <sanghee.cheon@kmov.kr>
2021-12-21 01:25:32 +0900

    user repository

---
+++ nest/src/users/user.repository.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { UserRequestDto } from './dto/user.request.dto';
+import { User } from './user.schema';
+
+@Injectable()
+export class UserRepository {
+  constructor(
+    @InjectModel(User.name)
+    private readonly userModel: Model<User>,
+  ) {}
+
+  async findUserByEmail(email: string): Promise<User | null> {
+    const user = await this.userModel.findOne({ email });
+    return user;
+  }
+
+  async findUserByIdWithoutPassword(userId: string): Promise<User | null> {
+    const user = await this.userModel.findById(userId).select('-password');
+    return user;
+  }
+
+  async existByEmail(email: string): Promise<boolean> {
+    const result = await this.userModel.exists({ email });
+    return result;
+  }
+
+  async create(user: UserRequestDto): Promise<User> {
+    return await this.userModel.create(user);
+  }
+}
